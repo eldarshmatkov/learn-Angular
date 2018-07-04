b60dba56e7b4700f8e98b0db663c66c1c8d5f89a
Eldar Shmatkov <eldarshmatkov@gmail.com>
2018-07-04 22:18:56 +0300

    components services etc.

---
+++ Angular Fundamentals/components/src/app/courses.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
+
+@Component({
+    selector: 'courses', // <courses>  // <div id="courses"> "#courses"
+    template: `
+    <h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor="let course of courses">
+            {{course}}
+        </li>
+    </ul>
+    `
+})
+
+export class CoursesComponent {
+    title="List of courses";
+    courses;
+
+    constructor(service: CoursesService) {
+        // let service = new CoursesService();// old realization
+        this.courses = service.getCourses();
+    }
+   
+}
