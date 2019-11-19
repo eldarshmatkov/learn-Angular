cd35ed8d24454c599a0d0ea9411cbef0d31832a0
Eldar Shmatkov <eldarshmatkov@gmail.com>
2019-11-19 14:23:44 +0300

    starting course custom project

---
+++ Course project app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simple a test', 'https://get.pxhere.com/photo/dish-meal-food-vegetable-recipe-cuisine-vegetarian-food-parmigiana-1417897.jpg'),
+        new Recipe('A Second Test Recipe', 'This is simple russian girl', 'https://cdn.pixabay.com/photo/2017/07/10/19/15/grilling-2491123_960_720.jpg')
+    ];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
